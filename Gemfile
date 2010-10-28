c5125c3cdf888d814134019ad19d0d842b0e3aba
Austin Schneider <soccer022483@gmail.com>
2010-10-28 14:12:25 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'haml', '3.0.22'
+gem 'haml-rails', '0.3.4'
+gem 'pg', '0.9.0'
+
+group :development, :test do
+  gem 'rspec-rails', '>= 2.0.1'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtgDeckBuilder::Application.config.secret_token = 'a82f9fe581f867bffda049beaa6a3db7e0fe54353ee76aa9ff00e8a0fd8321e44330817197e34e22d0a96a1bbae20d319a6d6efa33b82a2289cd78b6ed618cc6'
