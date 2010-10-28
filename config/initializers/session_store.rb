1e7f53d01dc60e598c7e5f9ef3cf920007d7f8ba
Austin Schneider <soccer022483@gmail.com>
2010-10-28 14:13:48 -0400

    rename

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MtgDeckBuilder::Application.config.session_store :cookie_store, :key => '_mtg_deck_builder_session'
+MtgDeckBuilder::Application.config.session_store :cookie_store, :key => '_mtg_deck_maker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
